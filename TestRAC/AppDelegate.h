33fe90674073c3191c0497f9f10e321a234b30b8
huangxin003 <huangxin003@bianfeng.com>
2016-10-06 23:07:18 +0800

    Initial Commit

---
+++ TestRAC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestRAC
+//
+//  Created by 黄鑫 on 16/10/6.
+//  Copyright © 2016年 Zhanqi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestRAC/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TestRAC
+//
+//  Created by 黄鑫 on 16/10/6.
+//  Copyright © 2016年 Zhanqi. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
